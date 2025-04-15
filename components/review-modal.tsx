841f053fc6ad1bfe8aae3ed252a31e9f5b7acb4f
Nlferu <wargacki.patryq@gmail.com>
2025-04-15 15:22:21 +0200

    placeholders for missing steps/modals implemented

---
+++ components/review-modal.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type ReviewModalProps = {
+  closeModal: () => void
+}
+
+const ReviewModal: React.FC<ReviewModalProps> = ({ closeModal }) => {
+  return <div>ReviewModal</div>
+}
+
+export default ReviewModal
---
+++ components/success-modal.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SuccessModal = () => {
+  return <div>SuccessModal</div>
+}
+
+export default SuccessModal
