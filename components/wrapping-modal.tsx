4d143df610ed4adda308c3be14da141a74150329
Nlferu <wargacki.patryq@gmail.com>
2025-04-15 17:53:43 +0200

    wrapping modal under development

--- components/wrapping-modal.tsx
+++ components/wrapping-modal.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
-const WrappingModal = () => {
-  return <div>WrappingModal</div>
+type WrappingModalProps = {
+  closeModal: () => void
+}
+
+const WrappingModal: React.FC<WrappingModalProps> = ({ closeModal }) => {
+  return (
+    <div className="w-full flex flex-col p-6">
+      <div className="mt-2">
+        {/* Top Section */}
+        <div className="flex justify-between items-center">
+          <div className="">Some Clock</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default WrappingModal
