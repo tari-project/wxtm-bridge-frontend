ac644c46b08d68883f2d19d738641f625e6adab3
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-04-30 18:27:54 +0200

    setting initial state moved to providers.tsx to avoid hydration warning. Header moved to page.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,7 @@
 import '@/styles/globals.css'
-import Header from '@/components/header'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Providers } from './providers'
-import { getConfig } from '@/utils/config'
-import { headers } from 'next/headers'
-import { cookieToInitialState } from 'wagmi'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,20 +23,13 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const headerList = await headers()
-  const cookieHeader = headerList.get('cookie')
-  const initialState = cookieToInitialState(getConfig(), cookieHeader)
-
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col items-center justify-center bg-[url('/background.png')]
         bg-no-repeat bg-cover bg-center min-h-screen`}
       >
-        <Providers initialState={initialState}>
-          <Header />
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   )
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,22 +1,27 @@
 'use client'
 
-import { ReactNode, useState } from 'react'
+import { ReactNode, useState, useEffect } from 'react'
 import { WagmiProvider, State } from 'wagmi'
 import { getConfig } from '@/utils/config'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { cookieToInitialState } from 'wagmi'
 
-export const Providers = (props: {
-  children: ReactNode
-  initialState?: State
-}) => {
+export const Providers = ({ children }: { children: ReactNode }) => {
   const [config] = useState(() => getConfig())
   const [queryClient] = useState(() => new QueryClient())
+  const [initialState, setInitialState] = useState<State | undefined>(undefined)
+
+  useEffect(() => {
+    const cookieHeader = document.cookie
+    const state = cookieToInitialState(config, cookieHeader)
+    setInitialState(state)
+  }, [config])
+
+  if (!initialState) return null
 
   return (
-    <WagmiProvider config={config} initialState={props.initialState}>
-      <QueryClientProvider client={queryClient}>
-        {props.children}
-      </QueryClientProvider>
+    <WagmiProvider config={config} initialState={initialState}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </WagmiProvider>
   )
 }
