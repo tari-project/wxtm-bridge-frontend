9479195565a01e94da04eb72ef991c6f7f091865
karczuRF <oskar.karcz@rumblefish.pl>
2025-05-21 15:25:11 +0200

    disable amount parsing to 6 decimals

--- utils/parse-wxtm-token-amount.ts
+++ utils/parse-wxtm-token-amount.ts
@@ -1,5 +1,3 @@
-import { parseUnits } from 'ethers'
-
 export function parseWxtmTokenAmount(amount: string): string {
   if (amount.split('.')[1] && amount.split('.')[1][0].length > 6) {
     throw new Error('Invalid amount: more than 6 decimal places')
@@ -7,6 +5,7 @@ export function parseWxtmTokenAmount(amount: string): string {
 
   const sanitizedAmount = amount.replace(/,/g, '')
 
-  const parsed = parseUnits(sanitizedAmount, 6)
-  return parsed.toString()
+  // commented out because the amount is parsed when sending transactions from the TU
+  // const parsed = parseUnits(sanitizedAmount, 6)
+  return sanitizedAmount
 }
