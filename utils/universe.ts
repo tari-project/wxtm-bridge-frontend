16f452494ed95f17758ead79541efb6979727018
karczuRF <oskar.karcz@rumblefish.pl>
2025-06-04 17:08:31 +0200

    cleanup

--- utils/universe.ts
+++ utils/universe.ts
@@ -1,7 +1,4 @@
 export const openExternalLink = (url: string, event: React.MouseEvent) => {
-  console.info('[ TAPPLET-BRIDGE ] open link url: ', url)
-  console.info('[ TAPPLET-BRIDGE ] open link event: ', { event })
-
   event.preventDefault()
   window.parent.postMessage({ type: 'open-external-link', url }, '*')
 }
