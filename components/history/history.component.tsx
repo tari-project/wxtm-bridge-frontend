225eec606717a05ddb0931ba0c9b80e2c7d1d89e
Nlferu <wargacki.patryq@gmail.com>
2025-06-25 16:47:45 +0200

    empty state and translations implemented for transactions history part, updated footer text to match newest designs

--- components/history/history.component.tsx
+++ components/history/history.component.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useRef, useState } from 'react'
 
 import useTariAccountStore from '@/store/account'
 
+import { useTranslation } from 'react-i18next'
 import { BridgeHistoryListItem } from '../transactions/BridgeListItem'
 import {
   HistoryListWrapper,
@@ -16,6 +17,7 @@ export const TransactionHistory: React.FC = ({}) => {
   const setDetailedTx = useTariAccountStore((s) => s.setDetailedTx)
   const targetRef = useRef<HTMLDivElement>(null)
   const [isScrolled, setIsScrolled] = useState(false)
+  const { t } = useTranslation('main', { useSuspense: false })
 
   useEffect(() => {
     const el = targetRef.current
@@ -35,19 +37,34 @@ export const TransactionHistory: React.FC = ({}) => {
             index={i}
             itemIsNew={i === 0}
             setDetailedTx={setDetailedTx}
+            isHistoryList={true}
           />
         )
       })}
     </ListItemWrapper>
   )
+
+  const emptyState = (
+    <div className="flex flex-col items-center justify-center h-full min-h-[40px] text-center leading-[200%] tracking-[-2%]">
+      <h3 className="text-sm font-medium text-black">
+        {t('no_transactions_found_yet')}!
+      </h3>
+      <p className="text-sm font-medium text-[#797979] mt-2">
+        {t('start_bridging_to_view')}.
+      </p>
+    </div>
+  )
+
   return (
-    <div className="bg-white/50 backdrop-blur-sm shadow-xl rounded-2xl p-4 mx-auto">
+    <div className="bg-white/50 backdrop-blur-sm shadow-xl rounded-2xl p-4 mx-auto min-h-[130px] fixed-box mb-5">
       <div className="flex flex-wrap items-center justify-between gap-4">
         <div className="w-full flex flex-col p-1">
           <div className="relative">
             <div className="flex items-center justify-center"></div>
             <HistoryListWrapper ref={targetRef}>
-              <ListWrapper>{listMarkup}</ListWrapper>
+              <ListWrapper>
+                {bridgeTxs && bridgeTxs.length > 0 ? listMarkup : emptyState}
+              </ListWrapper>
             </HistoryListWrapper>
           </div>
         </div>
