0d29abe1a421fa987b65eee4d98603fbef79ef42
karczuRF <oskar.karcz@rumblefish.pl>
2025-07-08 14:07:10 +0200

    add footer text component

--- components/main/main.component.tsx
+++ components/main/main.component.tsx
@@ -3,13 +3,12 @@
 import React, { useState } from 'react'
 
 import { MainComponentProps } from './main.types'
-import { openExternalLink } from '@/utils/universe'
-import { config } from '@/config'
 import { useTranslation } from 'react-i18next'
 import { TransactionHistory } from '../history/history.component'
 import { BridgeForm } from '../bridge-form/bridge-form.component'
 import { HomeText } from './home-text'
 import useTariAccountStore from '@/store/account'
+import { FooterText } from './footer-text'
 
 export const MainComponent: React.FC<MainComponentProps> = ({
   onConnectClick,
@@ -84,25 +83,7 @@ export const MainComponent: React.FC<MainComponentProps> = ({
           />
         )}
       </div>
-      <div className="fixed bottom-0 mb-4 left-0 w-full text-center text-xs text-black/50 items-center justify-center whitespace-pre-line leading-[200%]">
-        {t('bridge_one_way_notice')}{' '}
-        <a
-          onClick={(e) => openExternalLink(config.TARI_BRIDGE_FAQ_URL, e)}
-          rel="noopener noreferrer"
-          className="underline cursor-pointer font-normal text-black mt-2"
-        >
-          {t('see_faq')}
-        </a>
-        <span className="text-black"> | </span>
-        <a
-          onClick={(e) => openExternalLink(config.TARI_SC_AUDIT_URL, e)}
-          rel="noopener noreferrer"
-          className="underline cursor-pointer font-normal text-black"
-        >
-          {t('view_smart_contract_audit')}
-        </a>
-        .
-      </div>
+      <FooterText />
     </section>
   )
 }
